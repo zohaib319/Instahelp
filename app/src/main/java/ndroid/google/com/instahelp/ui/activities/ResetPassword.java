dedbfc82cb39542ebf6dc4a34c919ad2901b0bef
apple <zohaibahmad319@gmail.com>
2018-07-19 19:12:17 +0500

    update

--- app/src/main/java/ndroid/google/com/instahelp/ui/activities/ResetPassword.java
+++ app/src/main/java/ndroid/google/com/instahelp/ui/activities/ResetPassword.java
@@ -2,14 +2,38 @@ package ndroid.google.com.instahelp.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import ndroid.google.com.instahelp.R;
 
-public class ResetPassword extends AppCompatActivity {
+public class ResetPassword extends AppCompatActivity implements View.OnClickListener {
+    public EditText email;
+    public Button reset_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+        email = (EditText)findViewById(R.id.email);
+        reset_pass = (Button)findViewById(R.id.reset_password_btn);
+        reset_pass.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.reset_password_btn:
+                String mail = email.getText().toString();
+                FirebaseAuth.getInstance().sendPasswordResetEmail(mail);
+                Toast.makeText(this, "Password Reset Instructions sent to your email", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
     }
 }
--- app/src/main/java/ndroid/google/com/instahelp/ui/fragments/UserPreferenceFargment.java
+++ app/src/main/java/ndroid/google/com/instahelp/ui/fragments/UserPreferenceFargment.java
@@ -28,14 +28,7 @@ import ndroid.google.com.instahelp.models.SubCategories;
 
 import static android.content.ContentValues.TAG;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link UserPreferenceFargment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link UserPreferenceFargment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class UserPreferenceFargment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,14 +46,7 @@ public class UserPreferenceFargment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UserPreferenceFargment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static UserPreferenceFargment newInstance(String param1, String param2) {
         UserPreferenceFargment fragment = new UserPreferenceFargment();
@@ -159,16 +145,7 @@ public class UserPreferenceFargment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
